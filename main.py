38eaa5e94ea5503a60e899a600fec16b70078999
SAM OCHU <109700174+SamSpeedX@users.noreply.github.com>
2024-06-19 21:59:01 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+majibu = {
+  "hi":['hi there!', 'hello!']
+}
+
+def fikiria_jinsi_ya_jibu(swali):
+    swali = swali.lower()
+    jibu = majibu.get(swali, "repeat again.")
+    return random.choice(jibu)
+
+while True:
+    swali_lake = input("You: ")
+
+    jibu = fikiria_jinsi_ya_jibu(swali_lake)
+    print("bot: ", jibu)
